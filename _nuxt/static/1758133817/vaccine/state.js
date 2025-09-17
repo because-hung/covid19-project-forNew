7d6fdba182f7daf8ed9e11d04614a0a15383d126
LAPTOP-7M93HN2H\User <barryhung841212@gmail.com>
2025-09-18 02:35:34 +0800

    Updates

--- _nuxt/static/1758133817/vaccine/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcovid19-project-forNew\u002F_nuxt\u002Fstatic\u002F1758133817",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvaccine",config:{_app:{basePath:"\u002Fcovid19-project-forNew\u002F",assetsPath:"\u002Fcovid19-project-forNew\u002F_nuxt\u002F",cdnURL:a}}}}(null));
